1ed3a141716fbad0e66c7470dd7b61e7b5eff149
min <mjnorthwesternwork1121@gmail.com>
2019-03-18 17:14:20 -0500

    more

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import FriendMatchCard from "./components/FriendMatchCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
 import logo from './logo.svg';
 import './App.css';
+
+let rightGuess = 0;
+let topScore = 0;
+let pressComment = "Press on a friend to gain points! Click on the same one twice and you lose!";
+
 
 class App extends Component {
   render() {
